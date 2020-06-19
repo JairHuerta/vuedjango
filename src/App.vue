7a9135a4a46141ba35e99cd64269b6ad20443f8f
Jair Ramón Huerta <jair.17huerta@gmail.com>
2020-06-19 18:32:52 -0500

    Reinicio vue por errores

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,28 @@
 <template>
   <div id="app">
-    <Header />
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4">
-          <Categories />
-          <br>
-          <Types />
-        </div>
-        <div class="col-md-8">
-          <router-view></router-view>
-        </div>
-      </div>
-    </div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Types from "./components/Types";
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    Header,
-    Categories,
-    Types
+    HelloWorld
   }
-};
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
